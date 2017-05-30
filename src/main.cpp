8fd49f4240f7c8b7ab08fb8ae56971e0dad49dc4
Anton Yabchinskiy <arn@bestmx.net>
2017-05-30 16:18:26 +0300

    Remove GpxFile class

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "applicationui.hpp"
 #include "geopositionsource.hpp"
-#include "gpxfile.hpp"
 #include "gpxfilewriter.hpp"
 
 #include <bb/cascades/Application>
@@ -17,7 +16,6 @@ Q_DECL_EXPORT int main(int argc, char **argv)
     Application app(argc, argv);
 
     qmlRegisterType<GeoPositionSource>("a12n.geoPosition", 0, 1, "GeoPositionSource");
-    qmlRegisterType<GpxFile>("a12n.gpx", 0, 1, "GpxFile");
     qmlRegisterType<GpxFileWriter>("a12n.gpx", 0, 1, "GpxFileWriter");
 
     ApplicationUI appui;
