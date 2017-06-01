04b3aa15a4f0f55f82d0198583c383afaca0ee08
Anton Yabchinskiy <arn@bestmx.net>
2017-06-01 17:21:45 +0300

    Add deploy makefile
    To be used mainly to uninstall the application from command line.

---
+++ bb-deploy.mk
@@ -0,0 +1,24 @@
+DEV ?= 169.254.0.1
+PGK ?= arm/o.le-v7/gnsslogger.so.bar
+
+
+.PHONY: deploy install launch terminate uninstall
+
+
+install: CMD=-installApp
+install: deploy
+
+launch: CMD=-launchApp
+launch: deploy
+
+terminate: CMD=-terminateApp
+terminate: deploy
+
+uninstall: CMD=-uninstallApp
+uninstall: deploy
+
+
+deploy:
+	blackberry-deploy ${CMD}	\
+		-device ${DEV} -password ${PASS}	\
+		-package ${PKG}
