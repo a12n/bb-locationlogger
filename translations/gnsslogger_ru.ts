cb23af9b52fae487570df3a6af3685713ceab266
Anton Yabchinskiy <arn@bestmx.net>
2017-06-02 21:59:49 +0300

    Update Russian translation file

--- translations/gnsslogger_ru.ts
+++ translations/gnsslogger_ru.ts
@@ -11,7 +11,7 @@
         <location filename="../assets/MainPage.qml" line="154"/>
         <location filename="../assets/MainPage.qml" line="162"/>
         <source>m</source>
-        <translation type="unfinished">м</translation>
+        <translation>м</translation>
     </message>
     <message>
         <location filename="../assets/MainPage.qml" line="40"/>
@@ -19,27 +19,27 @@
         <location filename="../assets/MainPage.qml" line="143"/>
         <location filename="../assets/MainPage.qml" line="169"/>
         <source>m/s</source>
-        <translation type="unfinished">м/с</translation>
+        <translation>м/с</translation>
     </message>
     <message>
         <location filename="../assets/MainPage.qml" line="80"/>
         <source>Record</source>
-        <translation type="unfinished">Запись</translation>
+        <translation>Запись</translation>
     </message>
     <message>
         <location filename="../assets/MainPage.qml" line="90"/>
         <source>Recording is active</source>
-        <translation type="unfinished">Идёт запись</translation>
+        <translation>Идёт запись</translation>
     </message>
     <message>
         <location filename="../assets/MainPage.qml" line="91"/>
         <source>The application records location data to a GPX file. File will be finalized and closed.</source>
-        <translation type="unfinished">Приложение записывает данные в GPX файл. Запись будет остановлена и файл закрыт.</translation>
+        <translation>Приложение записывает данные в GPX файл. Запись будет остановлена и файл закрыт.</translation>
     </message>
     <message>
         <location filename="../assets/MainPage.qml" line="97"/>
         <source>Stop</source>
-        <translation type="unfinished">Стоп</translation>
+        <translation>Стоп</translation>
     </message>
 </context>
 <context>
@@ -82,33 +82,6 @@
         <location filename="../assets/SettingsPage.qml" line="67"/>
         <source>Save on Media Card</source>
         <translation>Сохранять на карту памяти</translation>
-    </message>
-</context>
-<context>
-    <name>main</name>
-    <message>
-        <source>m</source>
-        <translation type="obsolete">м</translation>
-    </message>
-    <message>
-        <source>m/s</source>
-        <translation type="obsolete">м/с</translation>
-    </message>
-    <message>
-        <source>Record</source>
-        <translation type="obsolete">Запись</translation>
-    </message>
-    <message>
-        <source>Recording is active</source>
-        <translation type="obsolete">Идёт запись</translation>
-    </message>
-    <message>
-        <source>The application records location data to a GPX file. File will be finalized and closed.</source>
-        <translation type="obsolete">Приложение записывает данные в GPX файл. Запись будет остановлена и файл закрыт.</translation>
-    </message>
-    <message>
-        <source>Stop</source>
-        <translation type="obsolete">Стоп</translation>
     </message>
 </context>
 </TS>
