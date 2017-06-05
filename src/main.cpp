0208e6aea0b2ed59e42f4e70dddc2888d9bbc100
Anton Yabchinskiy <arn@bestmx.net>
2017-06-05 22:38:41 +0300

    Set orgranization domain in application

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,7 @@ Q_DECL_EXPORT int main(int argc, char **argv)
 #endif // QT_NO_DEBUG
 
     app.setOrganizationName("a12n");
+    app.setOrganizationDomain("a12n.bitbucket.org");
     app.setApplicationName("gnsslogger");
 
     ApplicationUI appui;
