f123e0c4b6390ddca13503f18213185ef1b5b557
Anton Yabchinskiy <arn@bestmx.net>
2017-06-09 14:31:35 +0300

    Line numbers in translation files were updated

--- translations/gnsslogger_ru.ts
+++ translations/gnsslogger_ru.ts
@@ -4,80 +4,80 @@
 <context>
     <name>MainPage</name>
     <message>
-        <location filename="../assets/MainPage.qml" line="158"/>
-        <location filename="../assets/MainPage.qml" line="183"/>
-        <location filename="../assets/MainPage.qml" line="193"/>
+        <location filename="../assets/MainPage.qml" line="161"/>
+        <location filename="../assets/MainPage.qml" line="186"/>
+        <location filename="../assets/MainPage.qml" line="196"/>
         <source>m</source>
         <translation>м</translation>
     </message>
     <message>
-        <location filename="../assets/MainPage.qml" line="169"/>
-        <location filename="../assets/MainPage.qml" line="204"/>
+        <location filename="../assets/MainPage.qml" line="172"/>
+        <location filename="../assets/MainPage.qml" line="207"/>
         <source>m/s</source>
         <translation>м/с</translation>
     </message>
     <message>
-        <location filename="../assets/MainPage.qml" line="76"/>
+        <location filename="../assets/MainPage.qml" line="79"/>
         <source>Pause</source>
         <translation>Пауза</translation>
     </message>
     <message>
-        <location filename="../assets/MainPage.qml" line="87"/>
+        <location filename="../assets/MainPage.qml" line="90"/>
         <source>Record</source>
         <translation>Запись</translation>
     </message>
     <message>
-        <location filename="../assets/MainPage.qml" line="96"/>
+        <location filename="../assets/MainPage.qml" line="99"/>
         <source>Recording is active</source>
         <translation>Идёт запись</translation>
     </message>
     <message>
-        <location filename="../assets/MainPage.qml" line="97"/>
+        <location filename="../assets/MainPage.qml" line="100"/>
         <source>The application records location data to a GPX file. File will be finalized and closed.</source>
         <translation>Приложение записывает данные в GPX файл. Запись будет остановлена и файл закрыт.</translation>
     </message>
     <message>
-        <location filename="../assets/MainPage.qml" line="107"/>
+        <location filename="../assets/MainPage.qml" line="110"/>
         <source>Stop</source>
         <translation>Стоп</translation>
     </message>
     <message>
-        <location filename="../assets/MainPage.qml" line="128"/>
+        <location filename="../assets/MainPage.qml" line="131"/>
         <source>Location and Speed</source>
         <translation>Местоположение и скорость</translation>
     </message>
     <message>
-        <location filename="../assets/MainPage.qml" line="174"/>
+        <location filename="../assets/MainPage.qml" line="177"/>
         <source>Altitude and Vertical Speed</source>
         <translation>Высота и вертикальная скорость</translation>
     </message>
     <message>
-        <location filename="../assets/MainPage.qml" line="209"/>
+        <location filename="../assets/MainPage.qml" line="212"/>
         <source>Number of Satellites</source>
         <translation>Число спутников</translation>
     </message>
     <message>
-        <location filename="../assets/MainPage.qml" line="234"/>
+        <location filename="../assets/MainPage.qml" line="237"/>
         <source>Location services are disabled. Please enable location services and restart the application.</source>
         <translation>Службы определения местоположения отключены. Пожалуйста включите службы и перезапустите приложение.</translation>
     </message>
     <message>
-        <location filename="../assets/MainPage.qml" line="242"/>
+        <location filename="../assets/MainPage.qml" line="245"/>
         <source>No information reported from location services.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../assets/MainPage.qml" line="244"/>
+        <location filename="../assets/MainPage.qml" line="247"/>
         <source>Satellite signal lost. Try to get to a place with more sky visible.</source>
         <translation>Нет сигнала со спутников. Попробуйте выйти на более открытое место.</translation>
     </message>
     <message>
-        <location filename="../assets/MainPage.qml" line="246"/>
+        <location filename="../assets/MainPage.qml" line="249"/>
         <source>The device is stationary.</source>
         <translation>Устройство неподвижно.</translation>
     </message>
     <message>
-        <location filename="../assets/MainPage.qml" line="236"/>
+        <location filename="../assets/MainPage.qml" line="239"/>
         <source>Unknown error in location services. Try to restart the application.</source>
         <translation>Неизвестная ошибка в службах определения местоположения. Попробуйте перезапустить приложение.</translation>
     </message>
@@ -109,17 +109,17 @@
         <translation>мин</translation>
     </message>
     <message>
-        <location filename="../assets/SettingsPage.qml" line="54"/>
+        <location filename="../assets/SettingsPage.qml" line="60"/>
         <source>Stationary Detection</source>
         <translation>Исключать время остановок</translation>
     </message>
     <message>
-        <location filename="../assets/SettingsPage.qml" line="63"/>
+        <location filename="../assets/SettingsPage.qml" line="69"/>
         <source>Filter Location Data</source>
         <translation>Сглаживать данные</translation>
     </message>
     <message>
-        <location filename="../assets/SettingsPage.qml" line="72"/>
+        <location filename="../assets/SettingsPage.qml" line="78"/>
         <source>Save on Media Card</source>
         <translation>Сохранять на карту памяти</translation>
     </message>
